2045f82ea09af9f82fa862f16c43dd788dd6b3b1
Sean Foreman <sholynx@gmail.com>
2019-03-26 21:12:51 -0700

    Initial Commit

---
+++ GameTrkr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameTrkr
+//
+//  Created by Sean Foreman on 3/26/19.
+//  Copyright © 2019 Sean Foreman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

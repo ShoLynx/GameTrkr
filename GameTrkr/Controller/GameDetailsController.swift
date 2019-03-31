d463b1c68d5cb8f3cb1ec11fc528faefdc10726b
Sean Foreman <sholynx@gmail.com>
2019-03-30 18:32:19 -0700

    Add GameDetailsController

---
+++ GameTrkr/Controller/GameDetailsController.swift
@@ -0,0 +1,18 @@
+//
+//  GameDetailsController.swift
+//  GameTrkr
+//
+//  Created by Sean Foreman on 3/30/19.
+//  Copyright © 2019 Sean Foreman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GameDetailsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

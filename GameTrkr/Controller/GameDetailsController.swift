05721bfdd9a4aa1b3beee93b307a660d5505ecd0
Sean Foreman <sholynx@gmail.com>
2019-04-25 00:57:13 -0700

    Remove extra call for setupFetchedResultsController from viewDidLoad.

--- GameTrkr/Controller/GameDetailsController.swift
+++ GameTrkr/Controller/GameDetailsController.swift
@@ -50,14 +50,11 @@ class GameDetailsController: UIViewController {
     
     // MARK: Life cycle
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = platform.name! + " " + game.title!
         
-        setupFetchedResultsController()
         setCollectionFormat()
         
         pickerController.delegate = self
